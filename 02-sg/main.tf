dff79da60ac9416d402e7242fa235203071448eb
sandeepanainala <nainala_sandeep@yahoo.com>
2024-06-01 17:54:02 +0530

    terraform - expense project with VPN

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -42,7 +42,7 @@ module "app_alb" {
   source = "../../terraform-aws-securitygroup"
   project_name = var.project_name
   environment = var.environment
-  sg_description = "SG for app_alb instance"
+  sg_description = "SG for APP ALB Instances"
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = var.common_tags
   sg_name = "app_alb"
