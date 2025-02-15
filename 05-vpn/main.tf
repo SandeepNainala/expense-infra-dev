f7d0a8fa09304f61bbec3fd09765afc47dbd80ea
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-15 17:11:53 +0530

    Terraform - expense project, module vpn

--- 05-vpn/main.tf
+++ 05-vpn/main.tf
@@ -8,7 +8,7 @@ resource "aws_key_pair" "vpn" {
 
 module "vpn" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-
+  key_name = aws_key_pair.vpn.key_name
   name = "${var.project_name}-${var.environment}-vpn"
 
   instance_type          = "t3.micro"
