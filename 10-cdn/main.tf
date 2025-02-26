3c0da7711ab7e9b6245779682e35ba28daad92c1
sandeepanainala <nainala_sandeep@yahoo.com>
2025-02-26 10:57:12 +0530

    Terraform - expense project, module vpn

--- 10-cdn/main.tf
+++ 10-cdn/main.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_cloudfront_distribution" "web_cdn" {
   origin {
     domain_name = "web-${var.environment}-${var.zone_name}"  #web-dev.devops91.cloud
@@ -81,3 +82,4 @@ module "records" {
     }
   ]
 }
+*/
